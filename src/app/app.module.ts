e536a9dc060038d1145a016baed665f72e3f79a2
Vuhuy Pham <vuhuypham99@gmail.com>
2019-03-25 01:38:56 -0700

    Create ChatService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ChatComponent } from './chat/chat.component';
+import { ChatService } from './chat/chat.service';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,11 @@ import { ChatComponent } from './chat/chat.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     SlimLoadingBarModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ChatService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
